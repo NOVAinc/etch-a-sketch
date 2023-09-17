4bf00657856a8f60d338cf69cff2f2a877ef1632
Lucas <19545604+NOVAinc@users.noreply.github.com>
2023-09-17 04:06:51 -0300

    Add initial support for custom grid size

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-let areaGrid = [];
+// Grab container
+let gridContainer = document.getElementById("gridContainer");
 
-for (let i = 0; i < 16; i++) {
-    areaGrid.push(document.createElement("div"));
+let gridSize = 16;
+
+let gridAreas = [];
+
+function createGrid (gridSize) {
+    for (let i = 0, j = gridSize**2; i < j ; i++) {
+        let gridArea = document.createElement("div");
+        gridArea.innerText = `${i}`;
+        gridArea.classList.add("area");
+        gridAreas.push(gridArea);
+        gridContainer.appendChild(gridArea);
+        gridArea.addEventListener("mouseover", () => {
+            gridArea.style.backgroundColor = "#888888";
+        });
+    }
 }
 
-areaGrid.forEach(grid => grid.classList.add("area"));
+createGrid(gridSize);
 
-console.log(areaGrid);
