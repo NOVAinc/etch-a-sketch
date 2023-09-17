d539624ab490ef9935d761cb8d79315e65796b7a
Lucas <19545604+NOVAinc@users.noreply.github.com>
2023-09-17 04:30:24 -0300

    Add custom grid size button support

--- main.js
+++ main.js
@@ -5,19 +5,30 @@ let gridSize = 16;
 
 let gridAreas = [];
 
+function destroyGrid() {
+    gridAreas.forEach(area => {
+        gridContainer.removeChild(area)
+    });
+
+    gridAreas = [];
+}
+
 function createGrid (gridSize) {
     for (let i = 0, j = gridSize**2; i < j ; i++) {
         let gridArea = document.createElement("div");
-        gridArea.innerText = `${i}`;
+        //gridArea.innerText = `${i}`;
         gridArea.classList.add("area");
         gridAreas.push(gridArea);
         gridArea.style.height = `${100/gridSize}%`;
         gridArea.style.width = `${100/gridSize}%`;
+        gridArea.style.backgroundColor = "#111"
         gridContainer.appendChild(gridArea);
         gridArea.addEventListener("mouseover", () => {
             gridArea.style.backgroundColor = "#888888";
         });
     }
+
+    console.log(gridAreas);
 }
 
 createGrid(gridSize);
@@ -27,5 +38,8 @@ let gridChanger = document.getElementById("gridChanger");
 gridChanger.addEventListener("click", () => {
     do {
         gridSize = prompt("Enter a new size");
-    } while (Number.isNaN(gridSize));
+    } while (isNaN(gridSize) || gridSize > 100);
+
+    destroyGrid();
+    createGrid(gridSize);
 });
